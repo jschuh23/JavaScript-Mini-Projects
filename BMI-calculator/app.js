8ef451e678d00ba860b4d7fdb5945c29f14cc139
jschuh23 <janelleschuh@gmail.com>
2021-05-03 13:31:11 -0400

    Added base js

---
+++ BMI-calculator/app.js
@@ -0,0 +1,17 @@
+let weight = prompt('Please enter your weight in pounds.');
+let height = prompt('Please enter your height in inches. ');
+
+let bmi = weight / 2.2 / Math.pow(height / 39.37, 2);
+let bmiRound = Math.round(bmi);
+
+if (bmiRound < 18.5) {
+    alert(`Your BMI is ${bmiRound}, you are underweight.`);
+} else if (bmiRound >= 18.5 && bmiRound <= 24.9) {
+    alert(`Your BMI is ${bmiRound}, you have a normal weight.`);
+} else if (bmiRound >= 25 && bmiRound <= 29.9) {
+    alert(`Your BMI is ${bmiRound}, you are overweight.`);
+} else if (bmiRound >= 30 && bmiRound <= 34.9) {
+    alert(`Your BMI is ${bmiRound}, you are obese.`);
+} else if (bmiRound >= 35) {
+    alert(`Your BMI is ${bmiRound}, you are extremely obese.`);
+}
